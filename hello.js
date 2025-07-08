81f175cc35d1957a9ac45af639b9e957eb4d8c54
Nam <namht91203@gmail.com>
2025-07-08 19:22:13 +0700

    tinh chu vi dien tich hinh tron

--- hello.js
+++ hello.js
@@ -1,2 +1,10 @@
-console.log("Xin chào JavaScript!");
-console.log("Xin chào!");
+//bt4
+const pi = Math.PI;
+let r =Number(prompt("Nhập bán kính hình tròn:"));
+let s = pi * r **2;
+let c = 2 * pi * r;
+s = s.toFixed(2);
+c = c.toFixed(2);
+console.log("Diện tích hình tròn là: " + s);
+console.log("Chu vi hình tròn là: " + c);
+//bài 5
