b337b630e53c7797201d188aa5e204653373d03a
Nam <namht91203@gmail.com>
2025-07-08 19:39:06 +0700

    Start project

--- hello.js
+++ hello.js
@@ -8,3 +8,9 @@ c = c.toFixed(2);
 console.log("Diện tích hình tròn là: " + s);
 console.log("Chu vi hình tròn là: " + c);
 //bài 5
+let a = Number(prompt("Nhập số chiều dài:"));
+let b = Number(prompt("Nhập số chiều rộng:"));
+let dienTich = a * b;
+let chuVi = (a + b) * 2;
+console.log("Diện tích hình chữ nhật là: " + dienTich);
+console.log("Chu vi hình chữ nhật là: " + chuVi);
